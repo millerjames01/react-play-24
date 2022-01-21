e13b266088b35dc6e5b3d2508510c5d8b12b5459
Jim Miller <miller.james01@gmail.com>
2022-01-21 13:07:29 +0100

    Working with Reset button and Backspace button. Basic mobile view included in css.

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import math, { evaluate, isNumeric } from 'mathjs';
+import { evaluate, isNumeric } from 'mathjs';
 
 function App() {
   return (
@@ -16,7 +15,7 @@ function Nav() {
   return (
     <nav className="Nav">
       <a href="/" className="Nav-header">Play 24</a>
-      <a href="#" className="Nav-button">View Code</a> 
+      <a href="/#" className="Nav-button">View Code</a> 
     </nav>
   );
 }
@@ -31,6 +30,8 @@ class Game extends React.Component {
     }
     this.clickNumber = this.clickNumber.bind(this);
     this.clickOperator = this.clickOperator.bind(this);
+    this.reset = this.reset.bind(this);
+    this.backspace = this.backspace.bind(this);
   }
 
   clickNumber(number) {
@@ -49,61 +50,83 @@ class Game extends React.Component {
     }));
   }
 
+  reset() {
+    this.setState({
+      expression: "",
+      usedNumbers: [false, false, false, false]
+    });
+  }
+
+  backspace() {
+    var len = this.state.expression.length;
+    if(len > 0) {
+      var lastPress = this.state.expression.substring(len - 2, len - 1);
+      var result = evalCatchErrors(lastPress);
+      var usedNumbers = [...this.state.usedNumbers];
+      if(isNumeric(result)) {
+        var i = this.state.numbers.indexOf(result);
+        usedNumbers = [...this.state.usedNumbers];
+        usedNumbers[i] = !usedNumbers[i];
+        this.setState({
+          usedNumbers: usedNumbers
+        });
+      }
+      this.setState((state) => ({
+        expression: state.expression.substring(0, len - 2)
+      }));
+    }
+  }
+
   render() {
     return (
       <div className="Game">
-        <ResultArea usedNumbers={this.state.usedNumbers} expression={this.state.expression} />
+        <DisplayArea usedNumbers={this.state.usedNumbers} expression={this.state.expression} />
         <Controls numbers={this.state.numbers} usedNumbers={this.state.usedNumbers} 
-          clickNumber={this.clickNumber} clickOperator={this.clickOperator} />
+          clickNumber={this.clickNumber} clickOperator={this.clickOperator} reset={this.reset} backspace={this.backspace} />
       </div>
     );
   }
 }
 
-function ResultArea(props) {
-  var result;
-
-  try {
-    result = evaluate(props.expression);
-  } catch (e) {
-    result = "Still working...";
-  }
+function DisplayArea(props) {
+  var result = evalCatchErrors(props.expression);
 
   if(!isNumeric(result)) {
-    result = "Still working..."
+    result = "#";
   }
 
   var hasWon = result === 24 && props.usedNumbers.reduce((x, y) => x && y, true);
 
   return (
-    <div className="ResultArea">
-      <div>{props.expression} </div> <div>{' = ' + result}</div> {hasWon && (<div>You win!!!</div>) } 
+    <div className="DisplayArea">
+      <div className="DisplayArea-expression">
+        {(props.expression === "" && <span className="Gray">Combine the numbers to reach 24</span>) 
+        || props.expression }
+      </div>
+      <div className="DisplayArea-result">{result}</div> 
+      {hasWon && (<div className="DisplayArea-message">You win!!!</div>) } 
     </div>
   );
     
 }
 
-// Still working on this.
-class ShowResult extends React.Component {
-  cosntructor(props) {
-    super(props)
-    this.state.result = props.result;
-  }
-}
-
 function Controls(props) {
   return (
     <div className="Controls">
-      <div className="Controls-numberRow">
+      <div className="Controls-row">
         {props.numbers.map((number, index) => {
           var isUsed = props.usedNumbers[index];
           return <NumberButton key={index} number={number} isUsed={isUsed} handleClick={() => {props.clickNumber(number)}} />;
         })}
       </div>
-      <div className="Controls-operatorRow">
+      <div className="Controls-row">
         {['(', ')', '*', '+', '-', '/'].map((operator) => {
           return <OperatorButton key={operator} operator={operator} handleClick={() => {props.clickOperator(operator)}} />;
         })}
+      </div>
+      <div className="Controls-row">
+        <ResetButton  handleClick={() => {props.reset()}}/>
+        <BackspaceButton handleClick={() => {props.backspace()}}/>
       </div>
     </div>
   );
@@ -119,6 +142,22 @@ function NumberButton(props) {
 
 function OperatorButton(props) {
   return <div className="OperatorButton" onClick={props.handleClick}>{props.operator}</div>;
+}
+
+function ResetButton(props) {
+  return <div className="ResetButton" onClick={props.handleClick}>RESET</div>;
+}
+
+function BackspaceButton(props) {
+  return <div className="BackspaceButton" onClick={props.handleClick}>&lt;-</div>;
+}
+
+function evalCatchErrors(expr) {
+  try {
+    return evaluate(expr);
+  } catch (e) {
+    return '#';
+  }
 }
 
 export default App;
